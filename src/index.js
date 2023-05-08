e6c27b3d523fd0c03298969848e9586188468ac2
VYankov7 <vyankov02@gmail.com>
2023-05-08 22:19:05 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const el = document.getElementById('root');
-const root = ReactDOM.createRoot(el);
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
-root.render(<App/>)
+root.render(<App />);
